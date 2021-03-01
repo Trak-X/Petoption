6c97c84a42c3432f74436c4eea9ca7947ad9392c
Mike N. (R-WinOS) <mike@rndsoftwaregroup.com>
2021-03-01 14:31:08 +0200

    [4\x] DIEEEEEE KLint .. DIIIIIEEEEEE IN PAIN

--- app/src/main/java/com/example/androiddevchallenge/backend/Navigation.kt
+++ app/src/main/java/com/example/androiddevchallenge/backend/Navigation.kt
@@ -16,6 +16,6 @@
 package com.example.androiddevchallenge.backend
 
 sealed class Navigation(val destination:String){
-	object Dashboard : Navigation("Dashboard")
-	object PupDetails : Navigation("PupDetails")
+object Dashboard : Navigation("Dashboard")
+object PupDetails : Navigation("PupDetails")
 }
