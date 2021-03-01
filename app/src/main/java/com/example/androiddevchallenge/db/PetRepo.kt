2edc07a2bfbf682728efeec914491fee50b69588
Mike N. (R-WinOS) <mike@rndsoftwaregroup.com>
2021-03-01 14:17:28 +0200

    Updated projets klint rules [2\x] Fix all warning re function names

--- app/src/main/java/com/example/androiddevchallenge/db/PetRepo.kt
+++ app/src/main/java/com/example/androiddevchallenge/db/PetRepo.kt
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.example.androiddevchallenge.db
 
 import com.example.androiddevchallenge.R
